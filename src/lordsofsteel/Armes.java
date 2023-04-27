ff396f0a0f33b0a17ca44102deaf8a1f1af51456
ArnauMB <ArnauMB@DESKTOP-LF57AP7>
2023-04-27 11:40:26 +0200

    arma y personajes

---
+++ src/lordsofsteel/Armes.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lordsofsteel;
+
+/**
+ *
+ * @author ArnauMB
+ */
+public class Armes {
+    
+    protected String tipus;
+    protected int WPOW;
+    protected int WVEL; 
+
+    public Armes(String tipus, int WPOW, int WVEL){
+        
+    }
+    
+}
---
+++ src/lordsofsteel/Personatges.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lordsofsteel;
+
+/**
+ *
+ * @author ArnauMB
+ */
+public class Personatges {
+    
+    protected int FOR; 
+    protected int CON;
+    protected int VEL; 
+    protected int INT; 
+    protected int SOR;
+
+    protected Armes arma;
+    
+    public Personatges (int FOR, int CON, int VEL, int INT, int SOR){
+        this.FOR = FOR;
+        this.CON = CON;
+        this.VEL = VEL;
+        this.INT = INT;
+        this.SOR = SOR;
+        
+    }
+}
