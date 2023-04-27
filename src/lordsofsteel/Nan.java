7ba34100a888c35f11aa8fb7c09e7d4a323d3903
ArnauMB <ArnauMB@DESKTOP-LF57AP7>
2023-04-27 11:54:31 +0200

    NAN

--- src/lordsofsteel/Nan.java
+++ src/lordsofsteel/Nan.java
@@ -2,12 +2,15 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package miproyecto_;
+package lordsofsteel;
 
 /**
  *
  * @author ArnauMB
  */
-public class Nan {
+public class Nan extends Personatges{
     
+    public Nan (int FOR, int CON, int VEL, int INT, int SOR){
+        super(FOR, CON, VEL, INT, SOR);
+    }
 }
