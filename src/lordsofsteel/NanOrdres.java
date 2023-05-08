e91fb711d45baa7de52779710f7cdecb9d19c9aa
ArnauMB <ArnauMB@DESKTOP-LF57AP7>
2023-05-08 12:57:11 +0200

    ordre

---
+++ src/lordsofsteel/NanOrdres.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lordsofsteel;
+
+/**
+ *
+ * @author ArnauMB
+ */
+public class NanOrdres extends Nan implements Ordre{
+        
+    public NanOrdres (String NOM, int FOR, int CON, int VEL, int INT, int SOR, Armes arma){
+        super(NOM, FOR, CON, VEL, INT, SOR, arma);
+    }
+    public void restaurarPS(){
+        
+    }
+}
