89c38e8b5037f2daeb7de586b5e7998a6f89d13f
ArnauMB <ArnauMB@DESKTOP-LF57AP7>
2023-05-12 11:58:10 +0200

    principal combate

--- src/lordsofsteel/LordsofSteel.java
+++ src/lordsofsteel/LordsofSteel.java
@@ -12,36 +12,19 @@ import java.util.Scanner;
  * @author ArnauMB
  */
 public class LordsofSteel {
-///----prueba gitHub
     static Scanner sc = new Scanner(System.in);
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
- 
-        // Personatge normal: pd = (8+5)/4 = 3
-        // Nan: pd = (8+6+5)/4 = 4
-        // Nan: ps = (8+6) = 14
-        //System.out.println("Punts de dany de n1: " + n1.getPd());
-        //System.out.println("Punts de salut de n1: " + n1.getPs());
-        
-        /* Creació de personatges inicials */
-        //Arma arma = new Arma("Daga");
         Nan n1 = new Nan("Tirion",8,6, 13,16,16,new Armes("Daga"));        
         Huma h1 = new Huma("Luis",13,16,6,10,6,new Armes("Espasa"));
         Mitja mi1 = new Mitja("Frodo",9,7,7,15,16,new Armes("Martell"));
         Maia ma1 = new Maia("Nil",7,9, 12,15,17,new Armes("Daga"));
-        
-        /*Personatge[] personatges = new Personatge[4];
-        personatges[0] = n1;
-        personatges[1] = h1;*/
+
         ArrayList<Personatge> personatges = new ArrayList<Personatge>();
         personatges.add(n1);
         personatges.add(h1);
         personatges.add(mi1);
         personatges.add(ma1);
-                
-        
+
         /* Menú principal */
         System.out.println("");
         System.out.println("** MENÚ PRINCIPAL **");
@@ -50,7 +33,7 @@ public class LordsofSteel {
         System.out.println("3.- Editar personatge");
         System.out.println("4.- Iniciar combat");
         System.out.println("5.- Sortir");
-        System.out.println("");
+        System.out.println("**__________________**");
         System.out.print("Tria opció [1-5]: ");
         
         String entrada = sc.nextLine();
@@ -58,19 +41,22 @@ public class LordsofSteel {
         // Només hem d'arribar aquí si estem segurs
         // que l'opció triada és un enter entre 1-5
         int opcio = Integer.parseInt(entrada);
-           
-        
+              
         switch (opcio) {
             case 1:
+                agregaPersonatge();
                 break;
             case 2:
+                borrarPersonatge();
                 break;
             case 3:
+                editaPersonatge();
                 break;
             case 4:
                 iniciarCombat(personatges);
                 break;
             case 5:
+                System.out.println("¡Ha triat sortit!");
                 break;
         }
         
@@ -82,7 +68,6 @@ public class LordsofSteel {
         
         boolean[] seleccionats = new boolean[personatges.size()];
         Personatge[] lluitadors = new Personatge[2];
-        //for (Personatge p : personatges)
         for (int selec = 1; selec <= 2; selec++) {
             for (int i = 0; i < personatges.size(); ++i) {
                 if (!seleccionats[i]) {
@@ -113,7 +98,6 @@ public class LordsofSteel {
         Personatge atacant  = lluitadors[0];
         Personatge defensor = lluitadors[1];
         
-        
         Dau dau1 = new Dau();
         Dau dau2 = new Dau();
         Dau dau3 = new Dau();
@@ -132,8 +116,31 @@ public class LordsofSteel {
         Personatge aux = atacant;
         atacant  = defensor;
         defensor = aux;
+        if (valor <= atacant.getPA()) { // Atacant ataca
+            valor = dau1.llencar() + dau2.llencar() + dau3.llencar();
+            if (valor > defensor.getPE()) { // No aconsegueix esquivar
+                defensor.setPS(defensor.getPS() - atacant.getPD());
+            }
+        }
+        
+        Personatge ganador, perdedor;
+        if(defensor.getPS() <=0){
+            ganador = atacant;
+            perdedor = defensor;
+        }else{
+            ganador = defensor;
+            perdedor = atacant;
+        }
+        System.out.println("¡La batalla ha acabat!");
+        System.out.println("El guanyador es: "+ganador.getNOM());
+        System.out.println("------------------------");
+        
+        ganador.restaurarPS();
+        perdedor.restaurarPS();
+        /*revisar*/
+        
     }
-    //prueba
+
 }
  
 
